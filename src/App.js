8b9c6d97349dec5e258d8953f44593158de05812
Badrinarayan2003 <badrithamba@gmail.com>
2024-05-16 10:11:08 +0530

    changes on css property of project section

--- src/App.js
+++ src/App.js
@@ -5,30 +5,19 @@ import Skills from "./component/Skills";
 import Project from "./component/Project";
 import Contact from "./component/Contact";
 import Footer from "./component/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-
-        {/* <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/skills" element={<Skills />} />
-          <Route path="/projects" element={<Project />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes> */}
-        <Home />
-        <About />
-        <Skills />
-        <Project />
-        <Contact />
-        <Footer />
-      </>
-    </Router>
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Skills />
+      <Project />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
